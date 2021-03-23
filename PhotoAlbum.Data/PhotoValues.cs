a53ba4de11c62a59dc47f1a39319607430b03a16
crandles55 <rac35737@gmail.com>
2021-03-23 12:17:26 -0500

    initial commit

---
+++ PhotoAlbum.Data/PhotoValues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using PhotoAlbum.Data.models;
+
+namespace PhotoAlbum.Data
+{
+    public interface IPhotoValues
+    {
+        Task<Photo> GetPhotoValues();
+    }
+    
+    public class PhotoValues : IPhotoValues
+    {
+        public Task<Photo> GetPhotoValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhotoAlbum.Test/PhotoAlbumTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PhotoAlbum.Test
+{
+    public class PhotoAlbumTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
